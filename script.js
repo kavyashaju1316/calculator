a45dd226f609e1d5411c20b6a6da7a73e447120e
kavyashaju1316 <kavyashaju70@gmail.com>
2025-09-25 15:13:01 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let display = document.getElementById('result');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearScreen() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
